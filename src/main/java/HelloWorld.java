170cd205223c977ff9d8cada264dd92cdcec0eca
Pejman Ghorbanzade <mail@ghorbanzade.com>
2015-11-12 16:13:46 -0500

    fixed junit classpath problem

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,13 @@
-// package edu.umb.cs680;
+package edu.umb.cs680.hw01;
+
 public class HelloWorld {
-	public String sayHello() {
+	public static void main(String[] args) {
+		System.out.println(sayHello());
+	}
+	public static String sayHello() {
 		return "Hello World!";
+	}
+	public static String sayGoodBye() {
+		return "GoodBye!";
 	}
 }
--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,10 +1,22 @@
-// package edu.umb.cs680;
+package edu.umb.cs680.hw01;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
+
 public class HelloWorldTest {
 	@Test
-	public void testHellowWorld(){
+	public void testHelloWorld1(){
 		HelloWorld hello = new HelloWorld();
 		assertEquals("Hello World!", hello.sayHello());
+	}
+	@Test
+	public void testHelloWorld2(){
+		HelloWorld hello = new HelloWorld();
+		assertEquals("Hello World!", hello.sayHello());
+	}
+	@Test
+	public void testHelloWorld3(){
+		HelloWorld hello = new HelloWorld();
+		assertEquals("Good Bye!", hello.sayGoodBye());
 	}
 }
