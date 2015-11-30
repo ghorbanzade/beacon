78a1d5867108a2d8c28b28fdbf3b0fa093cfcaaa
Pejman Ghorbanzade <mail@ghorbanzade.com>
2015-11-30 14:06:19 -0500

    added unit tests for hw02, hw04 and hw06

--- src/test/java/hw06/StudentTuitionTest.java
+++ src/test/java/hw06/StudentTuitionTest.java
@@ -1,7 +1,7 @@
-package edu.umb.cs680.hw04;
+package edu.umb.cs680.hw06;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.instanceOf;
+// import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
@@ -30,25 +30,32 @@ public class StudentTuitionTest {
   }
 
   @Test
-  public void studentName() {
-    Student student = new Student("name", StudentStatus.OUTSTATE);
-    assertThat(student.getName(), is("name"));
+  public void makeInStateStudent() {
+    Student student = StudentFactory.createInStateStudent("name");
+    assertThat(student.getTuition(), is(1000F));
   }
 
   @Test
-  public void studentTuition() {
-    Student student = new Student("name", StudentStatus.OUTSTATE);
+  public void makeOutStateStudent() {
+    Student student = StudentFactory.createOutStateStudent("name");
     assertThat(student.getTuition(), is(2000F));
   }
 
   @Test
-  public void configInstance() {
-    assertThat(StudentConfig.getInstance(), is(instanceOf(StudentConfig.class)));
+  public void makeIntlStudent() {
+    Student student = StudentFactory.createIntlStudent("name");
+    assertThat(student.getTuition(), is(0F));
   }
 
   @Test
   public void getProperty() {
     StudentConfig sc = StudentConfig.getInstance();
     assertThat(sc.get("tuition.instate"), is("1000"));
+  }
+
+  @Test
+  public void getStudentName() {
+    Student student = new Student("name", StudentStatus.INSTATE);
+    assertThat(student.getName(), is("name"));
   }
 }
