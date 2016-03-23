e6379683841726e2de78183d14dd18d9fc04b7d2
Pejman Ghorbanzade <mail@ghorbanzade.com>
2016-03-23 01:46:05 -0400

    added solution to problem 7

---
+++ src/main/java/hw07/SummationMain.java
@@ -0,0 +1,47 @@
+//
+// CS681: Object Oriented Software Development
+// Copyright 2016 Pejman Ghorbanzade <mail@ghorbanzade.com>
+// Released under the terms of MIT License
+// https://github.com/ghorbanzade/UMB-CS681-2016S/blob/master/LICENSE
+//
+
+package edu.umb.cs681.hw07;
+
+/**
+ * This class demonstrates using a simple thread concurrent with
+ * the main thread.
+ *
+ * @author Pejman Ghorbanzade
+ * @see SummationRunnable
+ */
+public final class SummationMain {
+
+  /**
+   * This program initiates a thread to print numbers 10 to 0 while
+   * making sure that the thread is not destroyed by the main thread.
+   *
+   * @param args command line arguments
+   */
+  public static void main(String[] args) {
+    SummationRunnable runnable = new SummationRunnable(10);
+    Thread thread = new Thread(runnable);
+    thread.start();
+    for (int i = 0; i < 5; i++) {
+      System.out.printf("#%n");
+    }
+    runnable.setFlag();
+    try {
+      thread.join();
+      new Thread(runnable).start();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * this class should not be instantiated.
+   */
+  private SummationMain() {
+  }
+
+}
