00bb37f005229b156ec14a1cb336a4c84b134d27
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2019-02-23 20:43:02 -0600

    updated copyright header in all cs630 files

--- umb-cs630-2014f/src/sql/hw04/hw04q02-tables-drop.sql
+++ umb-cs630-2014f/src/sql/hw04/hw04q02-tables-drop.sql
@@ -1,8 +1,8 @@
 --
 -- CS630: Database Management Systems
--- Copyright 2014 Pejman Ghorbanzade <mail@ghorbanzade.com>
+-- Copyright 2014 Pejman Ghorbanzade <pejman@ghorbanzade.com>
 -- Creative Commons Attribution-ShareAlike 4.0 International License
--- More info: https://bitbucket.org/ghorbanzade/umb-cs630-2014f
+-- More info: https://github.com/ghorbanzade/beacon
 --
 
 DROP TABLE Enrolled;
